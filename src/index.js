645a1729aa4d03f0363dbb38da034ee58003c113
Your NameYuval <yuv.almaliah@gmail.com>
2021-11-12 12:30:26 +0200

    fix routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import ReactDom from 'react-dom';
 import React from 'react';
- import { Main } from './Main';
+import { Main } from './Main';
 import './style.css';
- 
- 
- const ROOT = document.getElementById('root');
- 
 
 
-  ReactDom.render(<Main />, ROOT);
- 
+const ROOT = document.getElementById('root');
+
+
+
+
+
+ReactDom.render(<Main />, ROOT);
